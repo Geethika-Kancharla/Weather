56f523363a6b13b15a3526b1cb468f98496cd388
Geethika-Kancharla <geethikak004@gmail.com>
2024-04-26 19:51:51 +0530

    Fetched api

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Weather.css'
+import { FaSearch } from "react-icons/fa";
 
 const Weather = () => {
-  return (
-    <div>Weather</div>
-  )
+
+    const [city, setCity] = useState("");
+    const [weather, setWeather] = useState();
+    const[error ,setError]=useState('');
+
+    const API_key = "4d18c9398c8b157bac9b4ea7c1b20e9e";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`;
+
+    const handleChange = (e) => {
+        setCity(e.target.value);
+        console.log(e.target.value);
+    }
+
+    async function fetchData() {
+        try {
+            const response=await fetch(url);
+            const output= await response.json();
+            if(response.ok){
+            setWeather(output);
+                setError('');
+                console.log(output);
+            }
+            else{
+                setError("No data found.Please enter a valid city name.");
+            }
+        }
+        catch (err) {
+            setError(err);
+        }
+    }
+
+    return (
+        <div className="container">
+            <div className='city'>
+                <input type='text' value={city} onChange={handleChange} placeholder='Enter the City Name' />
+                <button onClick={()=>fetchData()}><FaSearch /></button>
+            </div>
+
+        </div>
+    )
 }
 
 export default Weather
