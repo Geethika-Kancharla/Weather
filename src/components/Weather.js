51a5c6243af2ca5598f27bf468529b2b9a5eccbc
Geethika-Kancharla <geethikak004@gmail.com>
2024-04-26 22:09:57 +0530

    Updated gitignore

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
 import './Weather.css'
 import { FaSearch } from "react-icons/fa";
-import {API_key} from '../config'
+import { API_key } from '../config'
 
 const Weather = () => {
 
     const [city, setCity] = useState("");
     const [weather, setWeather] = useState();
-    const[error ,setError]=useState('');
+    const [error, setError] = useState('');
 
-    // const API_key = "4d18c9398c8b157bac9b4ea7c1b20e9e";
+
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`;
 
     const handleChange = (e) => {
@@ -19,14 +19,14 @@ const Weather = () => {
 
     async function fetchData() {
         try {
-            const response=await fetch(url);
-            const output= await response.json();
-            if(response.ok){
-            setWeather(output);
+            const response = await fetch(url);
+            const output = await response.json();
+            if (response.ok) {
+                setWeather(output);
                 setError('');
                 console.log(output);
             }
-            else{
+            else {
                 setError("No data found.Please enter a valid city name.");
             }
         }
@@ -39,11 +39,11 @@ const Weather = () => {
         <div className="container">
             <div className='city'>
                 <input type='text' value={city} onChange={handleChange} placeholder='Enter the City Name' />
-                <button onClick={()=>fetchData()}><FaSearch /></button>
+                <button onClick={() => fetchData()}><FaSearch /></button>
             </div>
-        {
-            error && <p className='error-message'>{error}</p>
-        }
+            {
+                error && <p className='error-message'>{error}</p>
+            }
         </div>
     )
 }
