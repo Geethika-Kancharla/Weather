89e8bed64b4fcb776857f480bc2fce20311600d5
Geethika-Kancharla <geethikak004@gmail.com>
2024-04-27 12:03:49 +0530

    Final Commit

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import './Weather.css'
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaWind } from "react-icons/fa";
+import { MdLocationOn } from 'react-icons/md'
 import { API_key } from '../config'
+import { WiHumidity } from 'react-icons/wi'
 
 const Weather = () => {
 
@@ -9,8 +11,7 @@ const Weather = () => {
     const [weather, setWeather] = useState();
     const [error, setError] = useState('');
 
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_key}`;
 
     const handleChange = (e) => {
         setCity(e.target.value);
@@ -43,6 +44,40 @@ const Weather = () => {
             </div>
             {
                 error && <p className='error-message'>{error}</p>
+            }
+            {
+                weather && weather.weather &&
+                <div className='content'>
+                    <div className='weather-image'>
+                        <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+                        <div className='desc'>{weather.weather[0].description}</div>
+                    </div>
+                    <div className='weather-temp'>
+                        <h2>{weather.main.temp}<span>&deg;C</span></h2>
+                    </div>
+                    <div className='weather-city'>
+                        <div className='location'>
+                            <MdLocationOn />
+                        </div>
+                        <p>{weather.name},<span>{weather.sys.country}</span></p>
+                    </div>
+                    <div className='weather-stats'>
+                        <div className='wind'>
+                            <div className='wind-icon'>
+                                <FaWind />
+                            </div>
+                            <h3 className='wind-speed'>{weather.wind.speed}<span>Km/h</span></h3>
+                            <h3 className='wind-heading'>Wind Speed</h3>
+                        </div>
+                        <div className='humidity'>
+                            <div className='humidity-icon'>
+                                <WiHumidity />
+                            </div>
+                            <h3 className='humidity-percent'>{weather.main.humidity}<span>%</span></h3>
+                            <h3 className='humidity-heading'>Humidity</h3>
+                        </div>
+                    </div>
+                </div>
             }
         </div>
     )
